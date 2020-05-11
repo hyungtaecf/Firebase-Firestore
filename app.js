c3f8d3af61dbe69fa3aebe83282d217b0b18a2c9
Hyung Tae C. Figur <hyu_03@hotmail.com>
2020-05-10 22:17:51 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const cafeList = document.getElementById('cafe-list');
+const form = document.getElementById('add-cafe-form');
+
+// create element and render cafe
+function renderCafe(doc) {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+    let cross = document.createElement('div');
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+    cross.textContent = 'x';
+
+    li.appendChild(name);
+    li.appendChild(city);
+    li.appendChild(cross);
+
+    cafeList.appendChild(li);
+
+    //deleting data
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    })
+}
+
+//getting data
+// db.collection('cafes')
+//     .where('city', '==', 'Manchester')
+//     .orderBy('name')
+//     .get().then((snapshot) => {
+//         snapshot.docs.forEach(doc => {
+//             renderCafe(doc);
+//         })
+//     })
+
+//saving data
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); //Prevents refreshing page when pressing the button
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
+})
+
+// real=time listener
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change => {
+        if (change.type == 'added') {
+            renderCafe(change.doc)
+        } else if (change.type == 'removed') {
+            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+            cafeList.removeChild(li);
+        }
+    })
+})
